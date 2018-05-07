3e8216e6b6cdfbc71139a4b0aaf80d436d770008
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-07 17:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ if (itWorked !== 3) {
 
 function placeOrder(cardNumber) {
   if (typeof cardNumber == 'number') {
-    return "you owe us money!"
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}`
   } else {
     return "Sorry, we don't have a credit card on file for you."
   }
