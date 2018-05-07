5285db6a1322d2f4c9be4942324b440c021b7fb3
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-07 17:16:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
   if (item === cart[i].itemName) {
     cart.splice(i, 1)
     var itWorked = 3
-    } 
+    }
   }
 if (itWorked !== 3) {
   return "That item is not in your cart."
