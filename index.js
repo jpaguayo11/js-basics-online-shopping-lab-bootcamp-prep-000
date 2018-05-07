1a142cc1770663f28a73859d90fd652e348c3ff7
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-07 17:43:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function removeFromCart(item) {
   for (var i = 0; i <cart.length; i++) {
   if (item === cart[i].itemName) {
     cart.splice(i, 1)
+    var itWorked = 3
     }
   }
 if (itWorked !== 3) {
